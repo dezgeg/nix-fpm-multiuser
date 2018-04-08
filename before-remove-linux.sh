19b4643b7df5364478d166ea733b1a7e127d12c5
Tuomas Tynkkynen <tuomas.tynkkynen@iki.fi>
2018-04-08 15:25:21 +0300

    Don't discard stderr in pre-remove

--- before-remove-linux.sh
+++ before-remove-linux.sh
@@ -3,6 +3,6 @@ set -e
 
 # Stop and disable the Nix daemon.
 if command -v systemctl >/dev/null 2>&1; then
-  systemctl stop nix-daemon.socket nix-daemon.service 2>/dev/null || true
-  systemctl disable nix-daemon.socket nix-daemon.service 2>/dev/null || true
+  systemctl stop nix-daemon.socket nix-daemon.service || true
+  systemctl disable nix-daemon.socket nix-daemon.service || true
 fi
