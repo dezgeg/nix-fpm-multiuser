e953cd21cd6d2e9a29ee7057c831aeb99287ab14
Tuomas Tynkkynen <tuomas.tynkkynen@iki.fi>
2018-04-08 15:24:13 +0300

    Use `systemctl enable --quiet`

--- after-install-linux.sh
+++ after-install-linux.sh
@@ -45,6 +45,6 @@ ln -sfn /opt/nix-multiuser/nix "$localstatedir/gcroots/nix-multiuser"
 
 # Finally, start nix-daemon.
 if command -v systemctl >/dev/null 2>&1; then
-  systemctl enable nix-daemon.socket || true
+  systemctl enable --quiet nix-daemon.socket || true
   systemctl start nix-daemon.socket || true
 fi
